23a94936e93543fa1b8807a694ef83d411c52712
Bhagyashri Vyawhare <150170466+bhagyashrivyawhare@users.noreply.github.com>
2026-05-16 16:55:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
-
-emailjs.init(emailConfig.publicKey);
+window.emailjs.init(emailConfig.publicKey);
 
 export { 
     doc, setDoc, getDoc, collection, addDoc, query, where, getDocs, updateDoc, 
@@ -23,6 +22,7 @@ export function getThumbnail(url) {
         if(url.includes('v=')) id = url.split('v=')[1].split('&')[0];
         else if(url.includes('youtu.be/')) id = url.split('/').pop().split('?')[0];
         else if(url.includes('/shorts/')) id = url.split('/shorts/')[1].split('?')[0];
+        else if(url.includes('/live/')) id = url.split('/live/')[1].split('?')[0];
         return id ? `https://img.youtube.com/vi/${id}/mqdefault.jpg` : 'https://res.cloudinary.com/dowhvdkjh/image/upload/v1777895769/IMG-20260504-WA0002_fucbjd.jpg';
     } catch(e) { return 'https://res.cloudinary.com/dowhvdkjh/image/upload/v1777895769/IMG-20260504-WA0002_fucbjd.jpg'; }
 }
