827e2862f592fe87518cb71b48d27af28f754e88
Bhagyashri Vyawhare <150170466+bhagyashrivyawhare@users.noreply.github.com>
2026-05-16 17:02:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
-window.emailjs.init(emailConfig.publicKey);
+
+emailjs.init(emailConfig.publicKey);
 
 export { 
     doc, setDoc, getDoc, collection, addDoc, query, where, getDocs, updateDoc, 
